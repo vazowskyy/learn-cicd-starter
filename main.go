7c15e58f65e6e11a1bafc76c59d949636bb48e00
vazowskyy <m.kryczka03@gmail.com>
2025-11-27 16:31:48 +0100

    Failure check

--- main.go
+++ main.go
@@ -24,6 +24,11 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+    // this function does nothing
+    // and is called nowhere
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -36,7 +41,6 @@ func main() {
 	}
 
 	apiCfg := apiConfig{}
-
 	// https://github.com/libsql/libsql-client-go/#open-a-connection-to-sqld
 	// libsql://[your-database].turso.io?authToken=[your-auth-token]
 	dbURL := os.Getenv("DATABASE_URL")
@@ -95,4 +99,7 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+
+
 }
+
